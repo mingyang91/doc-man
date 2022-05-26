b10b1ded9b67e37530610548193c2fa29528af53
明扬 <my@famer.me>
2022-05-26 15:58:08 +0800

    [update] Refactor with scala3
    Signed-off-by: 明扬 <my@famer.me>

--- templates/src/main/scala/dev/famer/document/datatypes/Values.scala
+++ templates/src/main/scala/dev/famer/document/datatypes/Values.scala
@@ -1,10 +1,9 @@
 package dev.famer.document.datatypes
 
-class Values(private val underlying: Map[String, String]) {
-  def apply(key: String): String = underlying.getOrElse(key, s"$key unsetted")
-  private[famer] def getInstance: Map[String, String] = underlying
-}
+opaque type Values = Map[String, String]
+extension (values: Values)
+  def field(key: String): String = values.getOrElse(key, s"$key unsetted")
 
 object Values {
-  def apply(underlying: Map[String, String]): Values = new Values(underlying)
+  def apply(underlying: Map[String, String]): Values = underlying
 }
